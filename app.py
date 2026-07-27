ae587821784e462b553bb042b356697ecb40740c
minginyus2 <minginyus2@snu.ac.kr>
2026-07-27 21:44:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ import streamlit as st
 st.title("두 숫자 더하기 앱")
 
 # 사용자로부터 숫자 입력 받기
-num1 = st.number_input(
-    "첫 번째 숫자를 입력하세요",
-    value=0.0,
-    step=1.0
-)
+num1 = st.number_input("첫 번째 숫자를 입력하세요",value=0.0,step=1.0)
 
 num2 = st.number_input(
     "두 번째 숫자를 입력하세요",
